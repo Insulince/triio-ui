6e10c9e20dca92aecccc2f9d113b28e783a12191
cn130940 <karl.a.nuetzel@centene.com>
2020-02-07 14:19:02 -0600

    seleneium

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -1,14 +1,121 @@
-import { AppPage } from './app.po';
-
-describe('workspace-project App', () => {
+import { AppPage } from "./app.po";
+import {browser} from "protractor";
+describe("workspace-project App", () => {
   let page: AppPage;
 
-  beforeEach(() => {
+  beforeEach(async () => {
     page = new AppPage();
   });
 
-  it('should display welcome message', () => {
-    page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to app!');
+  describe("Home page", () => {
+
+    beforeEach(() => {
+      page.navigateTo();
+    });
+
+    it("Clicking the 'get started' button should navigate to register page", () => {
+      const startButton = page.startButton;
+      startButton.click();
+      expect(browser.getCurrentUrl()).toMatch(/^.*\/register$/);
+    });
+
+    it("Clicking the 'Login' button should navigate to login page", () => {
+      const loginButton = page.loginButton;
+      loginButton.click();
+      expect(browser.getCurrentUrl()).toMatch(/^.*\/login$/);
+    });
+
+    it("Clicking the 'Register' button should navigate to register page", () => {
+      const registerButton = page.registerButton;
+      registerButton.click();
+      expect(browser.getCurrentUrl()).toMatch(/^.*\/register$/);
+    });
   });
+
+  describe("Register Page", () => {
+
+    let passwordInput;
+    let strengthInfo;
+    let createAccountBtn;
+    let usernameInput;
+    let emailInput;
+
+    beforeAll(() => {
+      page.navigateTo("register");
+      passwordInput = page.passwordInput;
+      strengthInfo = page.strengthInfo;
+      createAccountBtn = page.createAccountButton;
+      usernameInput = page.registerUsername;
+      emailInput = page.registerEmail;
+    });
+
+    it("the create account button should be disabled when there is no input", () => {
+      expect(createAccountBtn.getAttribute('disabled')).toEqual('true');
+    });
+
+    it("should show weak indicator if password is weak", () => {
+      passwordInput.sendKeys("bad password");
+      expect(strengthInfo.getText()).toEqual("Too weak");
+    });
+
+    it("should show good indicator if password is good", () => {
+      passwordInput.sendKeys("Password101");
+      expect(strengthInfo.getText()).toEqual("Good");
+    });
+
+    it("should show excellent indicator if password is excellent", () => {
+      passwordInput.sendKeys("Password101!");
+      expect(strengthInfo.getText()).toEqual("Excellent");
+    });
+
+    it("should take you to the login page when you click sign in", () => {
+      const signInLink = page.signInLink;
+      signInLink.click();
+      expect(browser.getCurrentUrl()).toMatch(/^.*\/login$/);
+    });
+
+    xit("should eneable the create account button when all inputs are filled out correctly", () => {
+      usernameInput.sendKeys("Bjarne");
+      emailInput.sendKeys("Bjarne@gmail.com");
+      passwordInput.sendKeys("Password101!");
+      expect(createAccountBtn.getAttribute('disabled')).toEqual('false');
+    });
+
+  });
+
+  describe("Login Page", () => {
+
+    let loginEmailInput;
+    let loginPasswordInput;
+
+    beforeEach(() => {
+      page.navigateTo("login");
+      loginEmailInput = page.loginEmailInput;
+      loginPasswordInput = page.loginPasswordInput;
+    });
+
+    it("should redirect you to the help page if you click 'forgot your password'", () => {
+      const forgotPassword = page.forgotPassword;
+      forgotPassword.click();
+      expect(browser.getCurrentUrl()).toMatch(/^.*\/help$/);
+    });
+
+    it("should redirect you to the register page if you click 'create an account'", () => {
+      const createAccount = page.createAccount;
+      createAccount.click();
+      expect(browser.getCurrentUrl()).toMatch(/^.*\/register$/);
+    });
+
+    it("should show invalid indicator when there is an invalid username and password", () => {
+      loginEmailInput.sendKeys("bjarne");
+      loginPasswordInput.sendKeys("stroustrop");
+      let invalidLogin = page.invalidLogin;
+      let loginButton = page.actualLoginButton;
+      loginButton.click();
+      expect(invalidLogin.getText()).toEqual("Invalid email or password.");
+
+    });
+
+  });
+
 });
--- e2e/src/app.po.ts
+++ e2e/src/app.po.ts
@@ -1,11 +1,68 @@
 import { browser, by, element } from 'protractor';
 
 export class AppPage {
-  navigateTo() {
-    return browser.get('/');
+  navigateTo(location: string = "") {
+    return browser.get('/' + location);
   }
 
-  getParagraphText() {
-    return element(by.css('app-root h1')).getText();
+  get registerButton() {
+    return element(by.className("register-btn"));
   }
+
+  get loginButton() {
+    return element(by.className("login-btn"));
+  }
+
+  get startButton() {
+    return element(by.className("btn-home"));
+  }
+
+  get strengthInfo() {
+    return element(by.className("strength-info"));
+  }
+
+  get passwordInput() {
+    return element(by.className("password-input"));
+  }
+
+  get createAccountButton() {
+    return element(by.className("create-btn"));
+  }
+
+  get registerEmail() {
+    return element(by.className("register-email"));
+  }
+
+  get registerUsername() {
+    return element(by.className("register-username"));
+  }
+
+  get signInLink() {
+    return element(by.className("sign-in-link"));
+  }
+
+  get createAccount() {
+    return element(by.className("create-account"));
+  }
+
+  get forgotPassword() {
+    return element(by.className("forgot-password"));
+  }
+
+  get loginEmailInput() {
+    return element(by.className("login-email-input"));
+  }
+
+  get loginPasswordInput() {
+    return element(by.className("login-password-input"));
+  }
+
+  get invalidLogin() {
+    return element(by.className("invalid"));
+  }
+
+  get actualLoginButton() {
+    return element(by.className("actual-login-btn"));
+  }
+
 }
